182836cf2a796b2a05a266c3bbeb5fb39eed1877
ANATATAMUES <agtatamues@utpl.edu.ec>
2023-11-30 23:26:22 -0500

    Actualizacion de Roles

--- login/login.php
+++ login/login.php
@@ -2,7 +2,6 @@
 include_once("../sesion.php");
 include_once("../dbconnection.php");
 include_once("../variables.php");
-
 if (isset($_GET['cerrar_sesion'])) {
     session_unset();
     session_destroy();
@@ -34,23 +33,18 @@ else
                 break;
         } 
     }
-    
     elseif (isset($_POST['usuario']) && isset($_POST['password'])) {
        $usr = $_POST['usuario'];
        $pass = $_POST['password'];
-    
-        //$db = new Database();
-        $query = "SELECT u.*, r.nombre
+       $query = "SELECT u.*, r.nombre
         FROM usuario AS u 
         INNER JOIN rol AS r
             ON r.id = u.id_rol
         WHERE u.usuario = '{$usr}'";
-    
-        $result = mysqli_query($conn, $query);
+         $result = mysqli_query($conn, $query);
         $nusr = mysqli_num_rows($result);
         $dato_usr = mysqli_fetch_array($result);
-    
-        if (($nusr == 1) && (password_verify($pass,$dato_usr['clave']))) {
+         if (($nusr == 1) && (password_verify($pass,$dato_usr['clave']))) {
             $rol = $dato_usr['id_rol'];
             $id_usuario = $dato_usr['id_usuario'];
             $_SESSION['id_usuario'] = $id_usuario;
@@ -58,7 +52,6 @@ else
             switch ($_SESSION['rol']) {
                case $admin:
                 echo "Ingresa a menu administrador";
-              //  header("Location: ../admin/admin.php");
                 echo"<script>window.location.replace('../admin/admin.php');</script>";
                 break;
             case $rece:
@@ -85,21 +78,14 @@ else
 }?>
 <!doctype html>
 <html lang="es">
-
 <head>
     <title>Inicio de Sesión</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
-    <!-- <link href="//fonts.googleapis.com/css?family=Nunito:300,400,600,700,800,900&display=swap" rel="stylesheet"> -->
-
     <script src="https://kit.fontawesome.com/16c313bafd.js" crossorigin="anonymous"></script>
-
     <link rel="stylesheet" href="login.css">
-
 </head>
-
 <body> 
     <section class="ftco-section">
         <div class="container">
@@ -143,13 +129,9 @@ else
                             </button>
                             </div>
                             <div class="modal-body">
-
                                 <i class="" style="color: rgb(57, 160, 57)" id="modal_icon"></i>
-
                                 <p id="texto_modal">  </p>
-
                             </div>
-
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-primary" data-dismiss="modal">Aceptar</button>
                             </div>
@@ -158,12 +140,9 @@ else
                 </div>
                 <!--Modal: modalPush-->
     </section>
-
     <script src="../assets/js/jquery-3.6.0.min.js"></script>
     <script src="../assets/js/popper.js"></script>
     <script src="../assets/js/bootstrap.min.js"></script>
     <script src="../assets/js/main.js"></script>
-
 </body>
-
 </html>
