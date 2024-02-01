a99a3f702b4f1fd9001eb60a751a9a4dc01fc5af
ANATATAMUES <agtatamues@utpl.edu.ec>
2024-02-01 16:31:00 -0500

    limpieza de codigo pacientelistced

--- php/paciente/paciente-list-ced.php
+++ php/paciente/paciente-list-ced.php
@@ -1,6 +1,6 @@
 <?php
 
-include '../../dbconnection.php';
+include_once '../../dbconnection.php';
 
 if(isset($_POST['usuario'])) {
 
@@ -52,5 +52,3 @@ if(isset($_POST['usuario'])) {
   }
     echo $jsonstring;
 }
-
-?>
